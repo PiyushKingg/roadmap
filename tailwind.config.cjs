ca430d8ba2d21739b58ec921bc5ee0dac7f774bf
PiyushKingg <piyushthedev69@gmail.com>
2025-09-19 19:18:57 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,1 +1,16 @@
-
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+      },
+      colors: {
+        aurora1: '#5AC8FA',
+        aurora2: '#A66BFF',
+        cosmicbg: '#040414'
+      }
+    }
+  },
+  plugins: []
+}
